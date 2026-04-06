701917b18686bf9064090f17b6c866c89ef7c566
Anthony Leung <anthonyleung07@gmail.com>
2026-04-06 22:03:00 +0100

    quickfix

--- components/auth/auth-button.tsx
+++ components/auth/auth-button.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
-import { Button } from "../ui/button";
+import { Button } from "@/components/ui/button";
 import { createClient } from "@/lib/supabase/server";
-import { LogoutButton } from "./logout-button";
+import { LogoutButton } from "@/components/auth/logout-button";
 
 export async function AuthButton() {
   const supabase = await createClient();
